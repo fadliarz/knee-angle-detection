8ee80c43cef67ff07dc1fc29bac51e76e7c0fef1
fadliarz <97732966+fadliarz@users.noreply.github.com>
2023-12-03 11:26:07 +0700

    Create hsv.py

---
+++ hsv.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+image_hsv = None
+pixel = (20,60,80)
+file_path = "marvell.png"
+
+def pick_color(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        pixel = image_hsv[y,x]
+
+        upper =  np.array([pixel[0] + 10, pixel[1] + 10, pixel[2] + 40])
+        lower =  np.array([pixel[0] - 10, pixel[1] - 10, pixel[2] - 40])
+        print([x, y], pixel, lower, upper)
+
+        image_mask = cv2.inRange(image_hsv,lower,upper)
+        cv2.imshow("mask",image_mask)
+
+def main():
+    import sys
+    global image_hsv, pixel
+
+    image_src = cv2.imread(file_path)  
+    if image_src is None:
+        print ("Image is None...")
+        return
+    cv2.imshow("bgr",image_src)
+
+    cv2.namedWindow('hsv')
+    cv2.setMouseCallback('hsv', pick_color)
+
+    image_hsv = cv2.cvtColor(image_src,cv2.COLOR_BGR2HSV)
+    cv2.imshow("hsv",image_hsv)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
